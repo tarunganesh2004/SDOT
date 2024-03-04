ff9f20124193cc5192f141542dd02dad6c777dee
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-03-04 15:34:54 +0530

    queue

---
+++ Queue/queueUsingStacks.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+class Main {
+    public static void queueUsingTwoStacks() {
+        Stack<Integer> s1 = new Stack<>(); 
+    }
+    public static void main(String[] args) {
+        
+    }
+}
